7ff2a11872c4d58ad562656aff1a495ae8a25cc8
ryan.yang <ryan.yang@104.com.tw>
2024-10-29 07:24:07 +0800

    調整打包環境

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import vue from '@vitejs/plugin-vue'
 import {
   fileURLToPath, URL 
 } from 'url'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+      }
+    }
+  },
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
